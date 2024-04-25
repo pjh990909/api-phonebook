1c24f147d0c9dd439479e4d8dba7ca420c2c02f3
pjh990909 <pjh990909@naver.com>
2024-04-25 13:05:20 +0900

    안드로이드 연결배포

--- src/main/java/com/javaex/config/WebMvcConfig.java
+++ src/main/java/com/javaex/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // 경로
         .allowedMethods("GET", "POST", "PUT", "DELETE")
-        .allowedOrigins("http://localhost:8080")
+        .allowedOrigins("*")
         ;
     }
 }
